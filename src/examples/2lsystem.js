50d1c8d71c1c36a32b463f40bddfdaaa94d535cc
Hernan Rodriguez Colmeiro <me@peregrinogris.com.ar>
2017-07-22 19:32:13 -0300

    Initial LSystem object.

--- src/examples/2lsystem.js
+++ src/examples/2lsystem.js
@@ -5,7 +5,54 @@ import { iterate, nestAST } from '../lsystems/context';
 // Helper
 const makeTree = input => nestAST(parse(input).body).tree;
 
-const acropetal = {
+class LSystem {
+  constructor(system = {}) {
+    this.system = system;
+    this.setProgram('');
+  }
+  setProgram(program) {
+    this.program = program;
+    this.ast = parse(this.program);
+  }
+  iterate() {
+    this.program = iterate(this.ast, this.system);
+    this.ast = parse(this.program);
+  }
+  walk(visitor) {
+    interpret(this.ast, visitor);
+  }
+}
+
+const createVisitor = (turtle, length, angle) => {
+  const stack = [];
+  return {
+    PushState: function PushState() {
+      stack.push([turtle.position, turtle.direction]);
+      turtle.drawPath();
+    },
+    PopState: function PopState() {
+      const state = stack.pop();
+      turtle.drawPath();
+      turtle.penUp();
+      turtle.setXY(state[0][0], state[0][1]);
+      turtle.setHeading(state[1]);
+      turtle.penDown();
+    },
+    Module: function Module(node, params) {
+      // All modules are interpreted as Forward
+      turtle.forward(params.length > 0 ? length * params[0] : length);
+    },
+    Rotation: function Rotation(node, params) {
+      if (node.name === '+') {
+        turtle.left(params.length ? params[0] : angle);
+      } else {
+        turtle.right(params.length ? params[0] : angle);
+      }
+    },
+  };
+};
+
+const acropetal = new LSystem({
   productions: {
     A: [
       {
@@ -17,9 +64,9 @@ const acropetal = {
   },
   axiom: 'B[+A]A[-A]A[+A]A',
   ignores: ['+', '-'],
-};
+});
 
-const basipetal = {
+const basipetal = new LSystem({
   productions: {
     A: [
       {
@@ -31,49 +78,29 @@ const basipetal = {
   },
   axiom: 'A[+A]A[-A]A[+A]B',
   ignores: ['+', '-'],
-};
+});
 
 const renderSystem = (system, target, x, y, length) => {
   const iterations = 4;
-  const stack = [];
   const angle = 60;
   const turtle = new Tortuga(target, x, y, length);
+  const visitor = Object.assign(createVisitor(turtle, length, angle), {
+    Module: function Module(node, params) {
+      if (node.name === 'A') {
+        turtle.color('green');
+      } else {
+        turtle.color('red');
+      }
+      // All modules are interpreted as Forward
+      turtle.forward(params.length > 0 ? length * params[0] : length);
+    },
+  });
 
-  let program = '';
   turtle.size(2);
   turtle.color('#b756a4');
   for (let i = 0; i < iterations; i += 1) {
-    program = iterate(parse(program), system);
-    interpret(parse(program), {
-      PushState: function PushState() {
-        stack.push([turtle.position, turtle.direction]);
-        turtle.drawPath();
-      },
-      PopState: function PopState() {
-        const state = stack.pop();
-        turtle.drawPath();
-        turtle.penUp();
-        turtle.setXY(state[0][0], state[0][1]);
-        turtle.setHeading(state[1]);
-        turtle.penDown();
-      },
-      Module: function Module(node, params) {
-        if (node.name === 'A') {
-          turtle.color('green');
-        } else {
-          turtle.color('red');
-        }
-        // All modules are interpreted as Forward
-        turtle.forward(params.length > 0 ? length * params[0] : length);
-      },
-      Rotation: function Rotation(node, params) {
-        if (node.name === '+') {
-          turtle.left(params.length ? params[0] : angle);
-        } else {
-          turtle.right(params.length ? params[0] : angle);
-        }
-      },
-    });
+    system.iterate();
+    system.walk(visitor);
     turtle.drawPath();
 
     // Reset and move to the right
